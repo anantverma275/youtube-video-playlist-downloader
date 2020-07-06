a7766f3c7f7f6d65bee8aa18d35e6166f1c60aa2
anantverma275 <anantverma275@yahoo.com>
2020-07-06 22:27:37 +0530

    Added option to exit using -1

--- main.py
+++ main.py
@@ -2,54 +2,80 @@ import re
 import json
 import urllib.request
 from pytube import YouTube
+import sys
 
-api_key = 'API_Key'    # enter your api key here
+api_key = 'YOUR_API_KEY'    # enter your api key here
 
-playlist = input("Enter the playlist link")
+playlist = input("Enter the playlist link:\n")
 playlist_id = re.split('list=', playlist)[1]
 
 url = f'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId={playlist_id}&key={api_key}'
 json_url = urllib.request.urlopen(url)
 data = json.loads(json_url.read())
 
+print("\n\nEnter -1 at any input stage to abort further downloading.")
 
-
-def available_options(yt):
-    for i, value in enumerate(yt.streams, start = 1):
+def select_available_options(video):   
+    print("\nAvailaible option(s):")
+    for i, value in enumerate(video.streams, start = 1):
         print(str(i) + ". " + value.type.capitalize() + " Format: ." + value.subtype + ", Resolution: " + str(value.resolution) + ", Size: " + str(round(value.filesize_approx/1048576,2)) + "MB")
-
+    option = int(input("\nChoose the option you want (Press 0 to skip this video): "))
+    while option < -1 or option > len(video.streams):
+        option = int(input("Invalid Input. Please choose correct item number between {} and {}. Press 0 to skip, -1 to exit,\n\nChoose the option you want: ".format(1, len(video.streams))))
+    return option
+    
 def getVideoTitle(i, data):
-    title = i + ". " + data['items'][i]['snippet']['title']
+    title = str(i+1) + ". " + data['items'][i]['snippet']['title']
     return title
+
 def getVideoLink(i, data):
     link = "https://www.youtube.com/watch?v=" + data['items'][i]['snippet']['resourceId']['videoId']
     return link
 
+def downloadVideo(video, option):
+    print("\nDownloading: " + '" '+ title + "." + video.streams[option-1].subtype + '"'+ " ...")
+    video.streams[option-1].download(filename = title)
+    print("Download completed.\n")
+    
+def select_Subtitle(caps):
+    print("Subtitles are available for the video in the following languages.")
+    caps_list = list(caps.lang_code_index.values())
+    for i, sub in enumerate(caps_list,  start = 1):
+        print(str(i) + ". " + sub.name)
+    subs_option = int(input("Enter the item number to download that subtitle otherwise, press 0: "))
+    while subs_option < -1 or subs_option > len(caps):
+            subs_option = int(input("Invalid Input. Please choose correct item number between {} and {}. Press 0 to skip, -1 to exit,\n\nChoose the option you want: ".format(1, len(caps))))
+    return subs_option
 
+def downloadSubs(caps, option):
+    list(caps.lang_code_index.values())[subs_option-1].download(title = title)
+    print("\nDownload completed.\n")
+    
 for i in range(len(data['items'])):
     title = getVideoTitle(i, data)
     video_link = getVideoLink(i, data)
-    print(title)
-    print("\nAvailaible options:")
-    yt = YouTube(video_link)
-    available_options(yt)
-    o = int(input("\nChoose the option you want: "))
-    while o <= 0 or o > len(yt.streams):
-        o = int(input("Invalid Input. Please choose correct item number between {} and {}.\n\nChoose the option you want: ".format(1, len(yt.streams))))
-    print("\nDownloading: " + title + "...")
-    yt.streams[o-1].download(filename = title)
-    print("Download completed.\n")
-    caps = yt.captions
+
+    print("\n\nVideo Name: " + title)
+
+    video = YouTube(video_link)
+    option = select_available_options(video)
+    
+    if option == 0:
+        continue
+    elif option == -1:
+        print("Exiting..\nThanks for using,\nMade with <3 by Anant Verma")
+        sys.exit()
+    else:
+        downloadVideo(video, option)
+
+    caps = video.captions
     if(len(caps) >=1):
-        print("Subtitles are available for the video in the following languages.")
-        caps_list = list(caps.lang_code_index.values())
-        for i, sub in enumerate(caps_list,  start = 1):
-            print(str(i) + ". " + sub.name)
-        s = int(input("Enter the item number to download that subtitle otherwise, press 0.\n"))
-        while s < 0 or s > len(caps):
-            s = int(input("Invalid Input. Please choose correct item number between {} and {}.\n\nChoose the option you want: ".format(1, len(caps))))
-        if s == 0:
+        subs_option = select_Subtitle(caps)
+        if subs_option == 0:
             pass
+        elif subs_option == -1:
+            print("Thanks for using,\nMade with <3 by Anant Verma")
+            sys.exit()
         else:
-            caps_list[s-1].download(title = title)
-    print("Download completed.\n-\n"*50)
+            downloadSubs(caps, option)
+    print("-"*50)
